4e82076693eab15eacf8738c5673724c7c19edd1
muerterico <muertericobeats@gmail.com>
2022-12-28 02:48:24 +0500

    Initial commit

---
+++ NoDotMod.cs
@@ -0,0 +1,36 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+using UnityEngine;
+
+namespace NoDotMod
+{
+    public class NoDotMod : ModBehaviour
+    {
+        private void Awake()
+        {
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+        }
+
+        private void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(NoDotMod)} is loaded!", MessageType.Success);
+
+            // Get the New Horizons API and load configs
+            var newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            newHorizons.LoadConfigs(this);
+
+            // Example of accessing game code.
+            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            {
+                if (loadScene != OWScene.SolarSystem) return;
+                ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+                GameObject.Find("Reticule/Image").SetActive(false);
+            };
+
+            
+        }
+    }
+}
